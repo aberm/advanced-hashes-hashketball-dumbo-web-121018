16a03a0c1caef02549070fcf2fcdc73ac2bd6066
Menachem Aber <abermendel@gmail.com>
2018-11-28 01:27:18 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,15 +37,15 @@ def game_hash
           :blocks => 1,
           :slam_dunks => 15
         },
-        "Brook Lopez" => {
-          :number => 11,
-          :shoe => 17,
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
+        "Mason Plumlee" => {
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 12,
+          :assists => 6,
           :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15
+          :blocks => 8,
+          :slam_dunks => 5
         },
         
       }
