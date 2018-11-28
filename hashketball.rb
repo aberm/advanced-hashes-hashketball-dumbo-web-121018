0ab87f921f09d3e48cd1ddc9a2129e9c759ec097
Menachem Aber <abermendel@gmail.com>
2018-11-28 01:29:51 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,15 +63,15 @@ def game_hash
       :team_name => 'Charlotte Hornets',
       :colors => ["Turquoise", "Purple"],
       :players => {
-        "Alan Anderson" => {
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+        "Jeff Adrien" => {
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         },
         "Reggie Evans" => {
           :number => 30,
