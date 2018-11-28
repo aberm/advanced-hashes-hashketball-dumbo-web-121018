577fbd317461210bf23e1f8ea808eef6b26719bf
Menachem Aber <abermendel@gmail.com>
2018-11-28 01:26:49 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,15 +27,15 @@ def game_hash
           :blocks => 12,
           :slam_dunks => 7
         },
-        "Reggie Evans" => {
-          :number => 30,
-          :shoe => 14,
-          :points => 12,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 12,
-          :blocks => 12,
-          :slam_dunks => 7
+        "Brook Lopez" => {
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
         },
         
       }
