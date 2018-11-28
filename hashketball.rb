b84859d99acfc2092ec7f4e60020ffe4457494a4
Menachem Aber <abermendel@gmail.com>
2018-11-28 01:23:52 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,16 +4,25 @@ def game_hash
   
   game = {
     :home => {
+      :team_name => 'Brooklyn Nets',
+      :colors => ["Black", "White"],
+      :players => {
+        "Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 
+        }
+      }
+    },
+    :away => {
       :team_name => 'et',
       :colors => [aerg],
       :players => {
         
-      }
-    },
-    :away => {
-      
     }
   }
+  game
+end
 
 
 
